5a81e5c11852ab173a66ee6184b39f9efa4ed301
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-04-18 09:34:02 +0200

    initial empty

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotify-tools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    plantuml
+
+    # custom pkg groups
+    linuxPkgs
+    macosPkgs
+  ];
+}
